0ffa358b484b281898367c1f97d0530fe4b73b86
Michael Pauls <michael.pauls@mailbox.tu-berlin.de>
2015-10-28 23:43:02 +0100

    added starter

--- main/src/main/java/org/openbaton/autoscaling/Main.java
+++ main/src/main/java/org/openbaton/autoscaling/Main.java
@@ -1,13 +1,18 @@
 package org.openbaton.autoscaling;
 
+import org.openbaton.autoscaling.utils.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 
 /**
  * Created by mpa on 27.10.15.
  */
+
 public class Main {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ElasticityManager.class, args);
     }
 
