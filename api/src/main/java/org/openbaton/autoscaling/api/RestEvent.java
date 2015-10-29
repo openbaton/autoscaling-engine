8a446a9b220ba85dad023cc2354cd9ed75057054
Michael Pauls <michael.pauls@fokus.fraunhofer.de>
2015-10-29 02:58:55 +0100

    updated rest interface

--- api/src/main/java/org/openbaton/autoscaling/api/RestEvent.java
+++ api/src/main/java/org/openbaton/autoscaling/api/RestEvent.java
@@ -16,14 +16,19 @@
 
 package org.openbaton.autoscaling.api;
 
+import com.google.gson.*;
+import net.minidev.json.parser.JSONParser;
 import org.openbaton.autoscaling.catalogue.VnfrMonitor;
 import org.openbaton.autoscaling.core.ElasticityManagement;
 import org.openbaton.catalogue.mano.record.NetworkServiceRecord;
+import org.openbaton.catalogue.nfvo.Action;
 import org.openbaton.catalogue.nfvo.ApplicationEventNFVO;
 import org.openbaton.exceptions.NotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.GsonJsonParser;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -37,35 +42,40 @@ public class RestEvent {
 
 	private Logger log = LoggerFactory.getLogger(this.getClass());
 
-//    @Autowired
-//    private ElasticityManagement elasticityManagement;
-//
-//    @Autowired
-//    private VnfrMonitor vnfrMonitor;
-
-//    /**
-//     * Adds a new VNF software Image to the image repository
-//     *
-//     * @param msg : NSR to add for autoscaling
-//     */
-//    @RequestMapping(value = "", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-//    @ResponseStatus(HttpStatus.CREATED)
-//    public void create(@RequestBody String msg) throws NotFoundException {
-//        log.debug("========================");
-//        log.debug("nsr=" + msg);
-//    }
+    @Autowired
+    private ElasticityManagement elasticityManagement;
 
     /**
      * Adds a new VNF software Image to the image repository
      *
-     * @param applicationEventNFVO : NSR to add for autoscaling
+     * @param msg : NSR in payload to add for autoscaling
      */
     @RequestMapping(value = "", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    public void create(@RequestBody ApplicationEventNFVO applicationEventNFVO) throws NotFoundException {
+    public void create(@RequestBody String msg) throws NotFoundException {
         log.debug("========================");
-        log.debug("nsr=" + (NetworkServiceRecord) applicationEventNFVO.getPayload());
+        log.debug("msg=" + msg);
+        JsonParser jsonParser = new JsonParser();
+        JsonObject json = jsonParser.parse(msg).getAsJsonObject();
+        Gson mapper = new GsonBuilder().create();
+        Action action = mapper.fromJson(json.get("action"), Action.class);
+        log.debug("ACTION=" + action);
+        NetworkServiceRecord nsr = mapper.fromJson(json.get("payload"), NetworkServiceRecord.class);
+        log.debug("NSR=" + nsr);
+        elasticityManagement.activate(nsr);
     }
+
+//    /**
+//     * Adds a new VNF software Image to the image repository
+//     *
+//     * @param applicationEventNFVO : NSR to add for autoscaling
+//     */
+//    @RequestMapping(value = "", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+//    @ResponseStatus(HttpStatus.CREATED)
+//    public void create(@RequestBody ApplicationEventNFVO applicationEventNFVO) throws NotFoundException {
+//        log.debug("========================");
+//        log.debug("nsr=" + (NetworkServiceRecord) applicationEventNFVO.getPayload());
+//    }
 
     /**
      * Removes the Application from the Application repository
@@ -74,8 +84,9 @@ public class RestEvent {
      */
     @RequestMapping(value = "{nsrId}", method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.OK)
-    public void delete(@PathVariable("nsrId") String nsrId) throws NotFoundException {
+    public void delete(@PathVariable("nsrId") NetworkServiceRecord nsr) throws NotFoundException {
         log.debug("========================");
-        log.debug("nsr=" + nsrId);
+        log.debug("nsr=" + nsr);
+        elasticityManagement.deactivate(nsr);
     }
 }
