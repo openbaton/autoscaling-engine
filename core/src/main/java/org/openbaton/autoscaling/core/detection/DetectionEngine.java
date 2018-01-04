527e870d7b16547f7f96a7ce088c5bb8c0a7036b
Michael Pauls <michael.pauls@fokus.fraunhofer.de>
2018-01-04 19:44:37 +0100

    fea: use ceilometer plugin

--- core/src/main/java/org/openbaton/autoscaling/core/detection/DetectionEngine.java
+++ core/src/main/java/org/openbaton/autoscaling/core/detection/DetectionEngine.java
@@ -21,6 +21,7 @@
 package org.openbaton.autoscaling.core.detection;
 
 import org.openbaton.autoscaling.configuration.AutoScalingProperties;
+import org.openbaton.autoscaling.configuration.NfvoProperties;
 import org.openbaton.autoscaling.configuration.SpringProperties;
 import org.openbaton.catalogue.mano.common.AutoScalePolicy;
 import org.openbaton.catalogue.mano.common.ScalingAlarm;
@@ -28,11 +29,23 @@ import org.openbaton.catalogue.mano.descriptor.VirtualDeploymentUnit;
 import org.openbaton.catalogue.mano.record.VNFCInstance;
 import org.openbaton.catalogue.mano.record.VirtualNetworkFunctionRecord;
 import org.openbaton.catalogue.nfvo.Item;
+import org.openbaton.catalogue.nfvo.viminstances.BaseVimInstance;
+import org.openbaton.catalogue.nfvo.viminstances.OpenstackVimInstance;
 import org.openbaton.exceptions.MonitoringException;
 import org.openbaton.exceptions.NotFoundException;
+import org.openbaton.exceptions.VimDriverException;
 import org.openbaton.monitoring.interfaces.MonitoringPlugin;
 import org.openbaton.monitoring.interfaces.MonitoringPluginCaller;
 import org.openbaton.plugin.utils.RabbitPluginBroker;
+import org.openbaton.sdk.NFVORequestor;
+import org.openbaton.sdk.api.exception.SDKException;
+import org.openstack4j.api.OSClient;
+import org.openstack4j.api.exceptions.AuthenticationException;
+import org.openstack4j.core.transport.Config;
+import org.openstack4j.model.common.Identifier;
+import org.openstack4j.model.identity.v3.Region;
+import org.openstack4j.model.telemetry.Sample;
+import org.openstack4j.openstack.OSFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -58,14 +71,16 @@ public class DetectionEngine {
 
   @Autowired private ConfigurableApplicationContext context;
 
-  private MonitoringPlugin monitor;
+  //  private MonitoringPlugin monitor;
 
   //@Autowired
   private DetectionManagement detectionManagement;
 
-  @Autowired private AutoScalingProperties autoScalingProperties;
-
   @Autowired private SpringProperties springProperties;
+
+  @Autowired private NfvoProperties nfvoProperties;
+
+  @Autowired private AutoScalingProperties autoScalingProperties;
 
   @PostConstruct
   public void init() {
@@ -73,40 +88,40 @@ public class DetectionEngine {
   }
 
   public void initializeMonitor() throws NotFoundException {
-    log.info(
-        "Get monitoring plugin with following parameters: "
-            + autoScalingProperties.getRabbitmq().getBrokerIp(),
-        springProperties.getRabbitmq().getUsername(),
-        springProperties.getRabbitmq().getPassword(),
-        springProperties.getRabbitmq().getPort(),
-        "zabbix-plugin",
-        "zabbix",
-        autoScalingProperties.getRabbitmq().getManagement().getPort());
-    try {
-      monitor =
-          new MonitoringPluginCaller(
-              autoScalingProperties.getRabbitmq().getBrokerIp(),
-              springProperties.getRabbitmq().getUsername(),
-              springProperties.getRabbitmq().getPassword(),
-              springProperties.getRabbitmq().getPort(),
-              "/",
-              "zabbix-plugin",
-              "zabbix",
-              autoScalingProperties.getRabbitmq().getManagement().getPort(),
-              120000);
-    } catch (TimeoutException e) {
-      log.error(e.getMessage(), e);
-    } catch (IOException e) {
-      log.error(e.getMessage(), e);
-    }
+    //    log.info(
+    //        "Get monitoring plugin with following parameters: "
+    //            + autoScalingProperties.getRabbitmq().getBrokerIp(),
+    //        springProperties.getRabbitmq().getUsername(),
+    //        springProperties.getRabbitmq().getPassword(),
+    //        springProperties.getRabbitmq().getPort(),
+    //        "zabbix-plugin",
+    //        "zabbix",
+    //        autoScalingProperties.getRabbitmq().getManagement().getPort());
+    //    try {
+    //      monitor =
+    //          new MonitoringPluginCaller(
+    //              autoScalingProperties.getRabbitmq().getBrokerIp(),
+    //              springProperties.getRabbitmq().getUsername(),
+    //              springProperties.getRabbitmq().getPassword(),
+    //              springProperties.getRabbitmq().getPort(),
+    //              "/",
+    //              "zabbix-plugin",
+    //              "zabbix",
+    //              autoScalingProperties.getRabbitmq().getManagement().getPort(),
+    //              120000);
+    //    } catch (TimeoutException e) {
+    //      log.error(e.getMessage(), e);
+    //    } catch (IOException e) {
+    //      log.error(e.getMessage(), e);
+    //    }
   }
 
   public synchronized List<Item> getRawMeasurementResults(
       VirtualNetworkFunctionRecord vnfr, String metric, String period)
-      throws MonitoringException, NotFoundException {
-    if (monitor == null) {
-      initializeMonitor();
-    }
+      throws MonitoringException, NotFoundException, SDKException, VimDriverException {
+    //    if (monitor == null) {
+    //      initializeMonitor();
+    //    }
     ArrayList<Item> measurementResults = new ArrayList<Item>();
     ArrayList<String> hostnames = new ArrayList<String>();
     ArrayList<String> metrics = new ArrayList<String>();
@@ -127,7 +142,57 @@ public class DetectionEngine {
             + " on metric "
             + metric
             + ".");
-    measurementResults.addAll(monitor.queryPMJob(hostnames, metrics, period));
+
+    NFVORequestor nfvoRequestor =
+        new NFVORequestor(
+            "autoscaling-engine",
+            vnfr.getProjectId(),
+            nfvoProperties.getIp(),
+            nfvoProperties.getPort(),
+            "1",
+            nfvoProperties.getSsl().isEnabled(),
+            autoScalingProperties.getService().getKey());
+
+    List<BaseVimInstance> allVims = nfvoRequestor.getVimInstanceAgent().findAll();
+
+    for (VirtualDeploymentUnit vdu : vnfr.getVdu()) {
+      for (String vduName : vdu.getVimInstanceName()) {
+        for (BaseVimInstance vim : allVims) {
+          if (vim.getType().startsWith("openstack")) {
+            OpenstackVimInstance osVim = (OpenstackVimInstance) vim;
+            if (osVim.getName().equals(vduName)) {
+              OSClient os = authenticate(osVim);
+              List<? extends Sample> samples = os.telemetry().samples().list();
+              log.debug("Retrieved list of samples: " + samples);
+              for (Sample sample : samples) {
+                log.debug("Sample to check if requested: " + sample);
+                for (String hostname : hostnames) {
+                  log.debug("Looking for measurements of " + hostname);
+                  if (sample.getMetadata().containsKey("display_name")) {
+                    if (sample.getMetadata().get("display_name").equals(hostname)
+                        && metrics.contains(sample.getMeter())) {
+                      log.info("Found requested measurement: " + sample);
+                      Item item = new Item();
+                      item.setHostname(hostname);
+                      item.setHostId(sample.getMetadata().get("instance_id").toString());
+                      item.setValue(Float.toString(sample.getVolume()));
+                      item.setLastValue(Float.toString(sample.getVolume()));
+                      item.setMetric(sample.getMeter());
+                      measurementResults.add(item);
+                      log.info("Added new item:" + item);
+                      break;
+                    }
+                  }
+                }
+              }
+            }
+          } else {
+            log.warn("VimInstance type " + vim.getType() + " is not supported...");
+          }
+        }
+      }
+    }
+    //    measurementResults.addAll(monitor.queryPMJob(hostnames, metrics, period));
     if (hostnames.size() != measurementResults.size()) {
       throw new MonitoringException(
           "Requested amount of measurements is greater than the received amount of measurements -> "
@@ -216,5 +281,93 @@ public class DetectionEngine {
   public void sendAlarm(
       String projectId, String nsr_id, String vnfr_id, AutoScalePolicy autoScalePolicy) {
     detectionManagement.sendAlarm(projectId, nsr_id, vnfr_id, autoScalePolicy);
+  }
+
+  public OSClient authenticate(OpenstackVimInstance vimInstance) throws VimDriverException {
+
+    OSClient os;
+    Config cfg = Config.DEFAULT;
+    cfg = cfg.withConnectionTimeout(10000);
+    try {
+      if (isV3API(vimInstance)) {
+
+        Identifier domain =
+            vimInstance.getDomain() == null || vimInstance.getDomain().equals("")
+                ? Identifier.byName("Default")
+                : Identifier.byName(vimInstance.getDomain());
+        Identifier project = Identifier.byId(vimInstance.getTenant());
+
+        //        String[] domainProjectSplit = vimInstance.getTenant().split(Pattern.quote(":"));
+        //        if (domainProjectSplit.length == 2) {
+        //          log.trace("Found domain name and project id: " + Arrays.toString(domainProjectSplit));
+        //          domain = Identifier.byName(domainProjectSplit[0]);
+        //          project = Identifier.byId(domainProjectSplit[1]);
+        //        }
+
+        log.trace("Domain id: " + domain.getId());
+        log.trace("Project id: " + project.getId());
+
+        os =
+            OSFactory.builderV3()
+                .endpoint(vimInstance.getAuthUrl())
+                .scopeToProject(project)
+                .credentials(vimInstance.getUsername(), vimInstance.getPassword(), domain)
+                .withConfig(cfg)
+                .authenticate();
+        if (vimInstance.getLocation() != null
+            && vimInstance.getLocation().getName() != null
+            && !vimInstance.getLocation().getName().isEmpty()) {
+          try {
+            Region region =
+                ((OSClient.OSClientV3) os)
+                    .identity()
+                    .regions()
+                    .get(vimInstance.getLocation().getName());
+
+            if (region != null) {
+              ((OSClient.OSClientV3) os).useRegion(vimInstance.getLocation().getName());
+            }
+          } catch (Exception ignored) {
+            log.warn(
+                "Not found region '"
+                    + vimInstance.getLocation().getName()
+                    + "'. Use default one...");
+            return os;
+          }
+        }
+      } else {
+        os =
+            OSFactory.builderV2()
+                .endpoint(vimInstance.getAuthUrl())
+                .credentials(vimInstance.getUsername(), vimInstance.getPassword())
+                .tenantName(vimInstance.getTenant())
+                .withConfig(cfg)
+                .authenticate();
+        if (vimInstance.getLocation() != null
+            && vimInstance.getLocation().getName() != null
+            && !vimInstance.getLocation().getName().isEmpty()) {
+          try {
+            ((OSClient.OSClientV2) os).useRegion(vimInstance.getLocation().getName());
+            ((OSClient.OSClientV2) os).identity().listTokenEndpoints();
+          } catch (Exception e) {
+            log.warn(
+                "Not found region '"
+                    + vimInstance.getLocation().getName()
+                    + "'. Use default one...");
+            ((OSClient.OSClientV2) os).removeRegion();
+          }
+        }
+      }
+    } catch (AuthenticationException e) {
+      throw new VimDriverException(e.getMessage(), e);
+    }
+
+    return os;
+  }
+
+  private boolean isV3API(BaseVimInstance vimInstance) {
+    return vimInstance.getAuthUrl().endsWith("/v3")
+        || vimInstance.getAuthUrl().endsWith("/v3/")
+        || vimInstance.getAuthUrl().endsWith("/v3.0");
   }
 }
